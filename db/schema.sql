d70a3bed859fb0358f75724e3ffc57295de81b18
Petros Shumie <shumie.peter@gmail.com>
2019-12-05 16:22:22 -0500

    created config folder with connection.js file to be exported later

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,11 @@
 -- Create burgers_db
-create burgers_db;
+CREATE DATABASE burgers_db;
 -- Initialze the use of burgers_db in database
-use burgers_db;
+USE burgers_db;
 -- Create burgers tables 
-create table burgers (
-    id int not null auto_increment,
-    burger_name varchar(255) not null,
-    devoured boolean DEFAULT,
-    primary key (id)
+CREATE TABLE burgers (
+    id INT NOT NULL auto_increment,
+    burger_name VARCHAR(255) NOT NULL,
+    devoured BOOLEAN DEFAULT false,
+    PRIMARY KEY (id)
 );
-
